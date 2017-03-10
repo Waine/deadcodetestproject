5f65073e71bfa6b15a029b1613e18b5fa7a19b01
Evgeny Konovalov <eakonovalov@gmail.com>
2017-03-10 08:47:41 +0300

    initial

---
+++ src/main/java/com/eakonovalov/test/Class2.java
@@ -0,0 +1,21 @@
+package com.eakonovalov.test;
+
+/**
+ * Created by Administrator on 10.03.2017.
+ */
+public class Class2 {
+
+    private int i;
+    protected int j;
+    int k;
+    public int l;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+}
